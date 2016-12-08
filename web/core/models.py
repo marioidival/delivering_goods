dc6b343fb70be57bd12b75562917dd102e783236
Mario Idival <marioidival@gmail.com>
2016-12-08 15:43:39 -0300

    Create Map model

--- web/core/models.py
+++ web/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.find_route import FindRoute
+from core.process_text import TextRouteProcess
 
-# Create your models here.
+
+class Map(models.Model):
+    name = models.CharField(max_length=100)
+    mesh = models.TextField()
+
+    def save(self, *args, **kwargs):
+        # Remove all \r\n from mesh text
+        self.mesh = self.mesh.replace('\r\n', ' ')
+        return super(Map, self).save(*args, **kwargs)
+
+    def process_mesh(self, root, destination):
+        '''Process `mesh` of `map` and return shortest route with distance'''
+        text_processor = TextRouteProcess(self.mesh)
+        find_route = FindRoute(text_processor.routes())
+        return find_route.shortest_route(root, destination)
