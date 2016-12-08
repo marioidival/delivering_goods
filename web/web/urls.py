7cffa5cf97d636adc04eacf27f1f5d8e835f84db
Mario Idival <marioidival@gmail.com>
2016-12-08 16:32:28 -0300

    Setup cache system

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 from core.views import CreateMapView, mesh_view
 
 
 urlpatterns = [
     url(r'^api/map', CreateMapView.as_view(), name='create_map'),
-    url(r'^api/mesh', mesh_view, name='get_mesh'),
+    url(r'^api/mesh', cache_page(60 * 15)(mesh_view), name='get_mesh'),
 ]
